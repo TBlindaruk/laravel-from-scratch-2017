73cc8d1dd8f969eb2dd9e930c2b98a1e3bd356b4
benkabenko <alen@benkovic.net>
2017-05-16 17:35:44 +0200

    testing 101

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -5,9 +5,11 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\Post;
 
 class ExampleTest extends TestCase
 {
+    use DatabaseTransactions; //s ovim sve sto se za vrijeme testiranja spremi u bazu, odma se i brise
     /**
      * A basic test example.
      *
@@ -15,6 +17,31 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $this->assertTrue(true);
+        // Given I have two records in the database that are posts, and each one is posted a month apart.
+        $first = factory(Post::class)->create();
+
+        $second = factory(Post::class)->create([
+            'created_at' => \Carbon\Carbon::now()->subMonth() //kreiranje postova koji su minus jedan mjesec
+        ]);
+        // When I fetch the archives
+
+        $posts = Post::archives();
+
+        // Then the respnse should be in the proper format
+
+        //$this->assertCount(2, $posts);
+        $this->assertEquals([
+            [
+                "year" => $first->created_at->format('Y'),
+                "month" => $first->created_at->format('F'),
+                "published" => 1
+            ],
+
+            [
+                "year" => $second->created_at->format('Y'),
+                "month" => $second->created_at->format('F'),
+                "published" => 1
+            ]
+        ], $posts);
     }
 }
